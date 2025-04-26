3b1f791d48842538de19caead960c84050715249
niruthan-nps <niruthanparanthaman@gmail.com>
2025-04-26 21:56:53 +0530

    repository change

--- src/main/java/fitness/fitness/repository/WorkoutRepository.java
+++ src/main/java/fitness/fitness/repository/WorkoutRepository.java
@@ -4,5 +4,5 @@ package fitness.fitness.repository;
 import fitness.fitness.model.Workout;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface WorkoutRepository extends MongoRepository<Workout, String> {
-}
+//public interface WorkoutRepository extends MongoRepository<Workout, String> {
+//}
