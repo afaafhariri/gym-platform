f6f37ef838ac7792f559ed17c23cef5bca23380e
Afaaf Hariri <afaafhariri@icloud.com>
2025-04-26 15:00:52 +0530

    oauth data saved in mongodb

--- server/src/main/java/com/gymplatform/server/config/SecurityConfig.java
+++ server/src/main/java/com/gymplatform/server/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.gymplatform.server.config;
 
+import com.gymplatform.server.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -16,7 +20,10 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("http://localhost:3000", true) // Redirect to frontend after successful login
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                        .defaultSuccessUrl("http://localhost:3000", true)
                 )
                 .logout(logout -> logout
                         .logoutSuccessUrl("http://localhost:3000")
