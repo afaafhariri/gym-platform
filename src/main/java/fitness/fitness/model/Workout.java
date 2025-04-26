d9277faee8ae1d5e4e45847fb7fd63f8b3484245
niruthan-nps <niruthanparanthaman@gmail.com>
2025-04-26 21:53:39 +0530

    new

--- src/main/java/fitness/fitness/model/Workout.java
+++ src/main/java/fitness/fitness/model/Workout.java
@@ -24,7 +24,7 @@ public class Workout {
     // Getters and Setters
 
     public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    public void setId(String id) { this.id = id; } //this is good
 
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
